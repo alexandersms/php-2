a28f9b7f26a7ca07b97ee404bac2d39fb130d5bb
Xander Sms Mobutu <smsxander@gmail.com>
2019-06-03 10:36:34 +0200

    premier commit

--- index.php
+++ index.php
@@ -1,2 +1,25 @@
 <?php
+require("inc/header.php");
+?>
+
+<?php
+require("Classes/Produit.php");
 echo "Bonjour";
+
+$hamac = new Produit();
+$hamac->name = "Hamac";
+
+var_dump($hamac);
+
+$parasol = new Produit();
+$parasol->name = "Parasol";
+
+?>
+
+<h1>Liste des produits</h1>
+
+
+<?php
+require("inc/footer.php");
+?>
+
